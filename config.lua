4d3ea6f52c993fabf2b4878bcd878b983f7fcca1
ImmercityRP <immercityrp@gmail.com>
2021-07-16 01:13:59 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,5 +12,5 @@ Config.StartingAccountMoney = {bank = 25000}
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.EnableHud            = false -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.MaxWeight            = 250   -- the max inventory weight without backpack
-Config.PaycheckInterval     = 7 * 60000 -- how often to recieve pay checks in milliseconds
+Config.PaycheckInterval     = 24 * 60000 -- how often to recieve pay checks in milliseconds
 Config.EnableDebug          = false
