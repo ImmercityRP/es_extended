f7c9553ea60dacad07d8ef801a2da7ad643f42ff
ImmercityRP <immercityrp@gmail.com>
2021-09-15 22:18:04 -0400

    mf-inv

--- server/main.lua
+++ server/main.lua
@@ -516,9 +516,3 @@ AddEventHandler('esx:playerLogout', function(source,callback)
 	
 	TriggerClientEvent("esx:onPlayerLogout",source)
 end)
-
-AddEventHandler('txAdmin:events:scheduledRestart',function(eventData)
-	if eventData.secondsRemaining == 30 then
-	  exports["mf-inventory"]:saveInventories()
-	end
-  end)
