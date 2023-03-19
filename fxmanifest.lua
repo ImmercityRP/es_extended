6649f7edff8f0e8f7b71249a4f47804f2ffeefaa
ImmerCity RolePlay <82741759+ImmercityRP@users.noreply.github.com>
2023-03-19 07:31:39 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,16 +8,7 @@ version 'legacy'
 
 shared_scripts {
 	'locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/fr.lua',
 	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/cs.lua',
-	'locales/sc.lua',
-	'locales/tc.lua',
 
 	'config.lua',
 	'config.weapons.lua',
